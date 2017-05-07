350b7c01ef2a62dcfc6aa14a0efa34e61033fed4
preston <broberts9@gmail.com>
2017-05-07 14:50:02 -0700

    merging app.js code

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
-// your code here!
+
+function getTokens(rawText) {
+    return rawText.toLowerCase().split(/[ ,!.";:-]+/).filter(Boolean).sort();
+}
+
+function removeHiddenClass() {
+    $('dl').removeClass('hidden');
+}
+
+function totalWordCount() {
+    var text = $('#user-text').val();
+    var words = getTokens(text);
+    $('.js-word-count').html(words.length);
+}
+
+function uniqueWordCount() {
+    var text = $('#user-text').val();
+    var words = getTokens(text);
+    var wordCount = {};
+    var uniqueWords = 0;
+    for (var i = 0; i < words.length; i++) {
+        if (words[i] in wordCount) {
+            wordCount[words[i]]++;
+        }
+        else {
+            wordCount[words[i]]=1;
+        }
+    }
+    console.log(wordCount);
+    Object.keys(wordCount).forEach(function(key) {
+        if (key = 1) {
+            uniqueWords++;
+        }
+    })
+    $('.js-unique-count').html(uniqueWords);
+}
+
+function averageWordLength() {
+    var text = $('#user-text').val();
+    var words = getTokens(text);
+    var wordLength = {};
+    var wordLengthSum = 0;
+    for (var i = 0; i < words.length; i++) {
+        wordLength[words[i]] = words[i].length;
+        wordLengthSum += words[i].length;
+    }
+    console.log(wordLength);
+    console.log(wordLengthSum);
+    var average = wordLengthSum / words.length;
+    $('.js-average-length').html(average + " characters");
+}
+
+$('button').click(function(event) {
+    event.preventDefault();
+    removeHiddenClass();
+    totalWordCount();
+    uniqueWordCount();
+    averageWordLength();
+})
